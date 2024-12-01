84cf6d581eaceaa515635c641fd96c89bd790d67
quoc <phamminhquoccodinggithub@gmail.com>
2024-12-01 19:15:23 +0700

    Add scripts to compile and run C++

---
+++ compile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Get the directory of the script
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Check if a module name is provided
+if [ -z "$1" ]; then
+    echo "Please specify a module to compile (module1 or module2)."
+    exit 1
+fi
+
+MODULE=$1
+
+# Find all .cpp files in the src directory of the specified module
+SRC_FILES=$(find "$DIR/$MODULE" -name "*.cpp" 2>/dev/null)
+
+# Check if there are any source files
+if [ -z "$SRC_FILES" ]; then
+    echo "No source files found for module: $MODULE"
+    exit 1
+fi
+
+# Find all .cpp files in the tests directory of the specified module (if exists)
+if [ -d "$DIR/$MODULE/tests" ]; then
+    TEST_FILES=$(find "$DIR/$MODULE/tests" -name "*.cpp" 2>/dev/null)
+fi
+
+# Create bin directory if it does not exist
+mkdir -p "$DIR/$MODULE/bin"
+
+# Compile command
+if [ -z "$TEST_FILES" ]; then
+    g++ -fdiagnostics-color=always -g $SRC_FILES -o "$DIR/$MODULE/bin/main.out"
+else
+    g++ -fdiagnostics-color=always -g $SRC_FILES $TEST_FILES -o "$DIR/$MODULE/bin/main.out"
+fi
---
+++ compile_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+MODULE=$1
+# Call script2.sh
+./compile.sh $MODULE
+
+# Get the directory of the script
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Run binary file
+"$DIR/$MODULE/bin/main.out"
